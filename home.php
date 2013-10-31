df6765fbc8255d0da077269e2b9fe69e336cb2a9
axonus <axon@radiantbone.com>
2013-10-31 15:45:45 -0700

    rolled back file to fix ios issue

--- home.php
+++ home.php
@@ -1,13 +1,7 @@
-<?
-	$longitude	= $_REQUEST["longitude"];
-	$latitude	= $_REQUEST["latitude"];
-	$language	= $_REQUEST["language"];
-	$udid		= $_REQUEST["UDID"];
-
-//	echo "longitude = .$longitude.<BR>";
-//	echo "latitude = .$latitude.<BR>";
-//	echo "language = .$language.<BR>";
-//	echo "udid = .$udid.<BR>";
-
-echo "<SCRIPT LANGUAGE=\"javascript\">document.location.href=\"customerlogin.php?longitude=$longitude&latitude=$latitude&language=$language&udid=$udid\";</SCRIPT>";
+<?
+        $longitude        = $_REQUEST["longitude"];
+        $latitude        = $_REQUEST["latitude"];
+        $language        = $_REQUEST["language"];
+        $udid                = $_REQUEST["UDID"];
+        echo "<SCRIPT LANGUAGE=\"javascript\">document.location.href=\"customerlogin.php?longitude=$longitude&latitude=$latitude&language=$language&udid=$udid\";</SCRIPT>";
 ?>
