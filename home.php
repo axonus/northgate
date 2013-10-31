2e4421a0a9ec2d7015c102b067e2391f1b4647eb
axonus <axon@radiantbone.com>
2013-10-31 15:50:07 -0700

    jon's original update

--- home.php
+++ home.php
@@ -1,7 +1,25 @@
 <?
+
         $longitude        = $_REQUEST["longitude"];
+
         $latitude        = $_REQUEST["latitude"];
+
         $language        = $_REQUEST["language"];
+
         $udid                = $_REQUEST["UDID"];
-        echo "<SCRIPT LANGUAGE=\"javascript\">document.location.href=\"customerlogin.php?longitude=$longitude&latitude=$latitude&language=$language&udid=$udid\";</SCRIPT>";
+
+
+
+//        echo "longitude = .$longitude.<BR>";
+
+//        echo "latitude = .$latitude.<BR>";
+
+//        echo "language = .$language.<BR>";
+
+//        echo "udid = .$udid.<BR>";
+
+
+
+echo "<SCRIPT LANGUAGE=\"javascript\">document.location.href=\"customerlogin.php?longitude=$longitude&latitude=$latitude&language=$language&udid=$udid\";</SCRIPT>";
+
 ?>
